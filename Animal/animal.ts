9337184a3bd3431463c30023df31e082e00832ca
Nalin Athukorala <115796315+indrajithathukorala@users.noreply.github.com>
2023-01-29 20:57:37 +0530

    Update animal.ts

--- Animal/animal.ts
+++ Animal/animal.ts
@@ -10,4 +10,13 @@ let age: number = 37;
 let sentence: string = `Hello, my name is ${fullName}.
  
 I'll be ${age + 1} years old next month.`;
+
+let sentence: string =
+  "Hello, my name is " +
+  fullName +
+  ".\n\n" +
+  "I'll be " +
+  (age + 1) +
+  " years old next month.";
+
 }
