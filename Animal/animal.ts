bffa74a721438d53e7c125791e4d5cac71948d39
Nalin Athukorala <115796315+indrajithathukorala@users.noreply.github.com>
2023-01-29 20:57:11 +0530

    Update animal.ts

--- Animal/animal.ts
+++ Animal/animal.ts
@@ -4,4 +4,10 @@ let hex: number = 0xf00d;
 let binary: number = 0b1010;
 let octal: number = 0o744;
 let big: bigint = 100n;
+
+let fullName: string = `Bob Bobbington`;
+let age: number = 37;
+let sentence: string = `Hello, my name is ${fullName}.
+ 
+I'll be ${age + 1} years old next month.`;
 }
