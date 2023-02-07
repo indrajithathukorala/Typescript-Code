e07d928ea7b9269b4c0156d8ed1ef38463a71f01
indrajithathukorala <nalinathukoralanalin@gmail.com>
2023-02-07 10:23:06 +0530

    Second Commit

--- Animal/animal.ts
+++ Animal/animal.ts
@@ -1,22 +1,19 @@
 class Animal {
-   let decimal: number = 6;
-let hex: number = 0xf00d;
-let binary: number = 0b1010;
-let octal: number = 0o744;
-let big: bigint = 100n;
+     name() {
+        type animal = {
+            name:string
+        };
 
-let fullName: string = `Bob Bobbington`;
-let age: number = 37;
-let sentence: string = `Hello, my name is ${fullName}.
- 
-I'll be ${age + 1} years old next month.`;
+        type d = {
+            age:number
+        }
 
-let sentence: string =
-  "Hello, my name is " +
-  fullName +
-  ".\n\n" +
-  "I'll be " +
-  (age + 1) +
-  " years old next month.";
+        type all = animal | d;
 
+        let n: all = {
+            name:"do",
+             age:45
+        };
+        
+    }
 }
